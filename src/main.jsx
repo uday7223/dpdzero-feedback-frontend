edfa3027339bd141856ceecd946b1258f5c6b7d7
udaydevanur <uday.devnur223@gmail.com>
2025-06-27 15:03:43 +0530

    added login scss for login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx'
